a05a259e69e3bcc87310f7ec9507e9b8495b0926
12301068JiangDongyu <12301068@bjtu.edu.cn>
2017-05-16 20:21:56 +0800

    POST请求

--- src/main/java/com/pku/system/controller/LoginController.java
+++ src/main/java/com/pku/system/controller/LoginController.java
@@ -31,28 +31,9 @@ public class LoginController {
      * @return
      */
     @ApiOperation(value = "注册", notes = "注册notes", produces = "application/json")
-    @RequestMapping(value="/login",method = RequestMethod.GET)
-    public @ResponseBody
-    String login(User user, HttpServletRequest request){
-//        Subject subject = SecurityUtils.getSubject();//使用shiro
-//        UsernamePasswordToken token = new UsernamePasswordToken(user.getUsername(),user.getPassword());
-//        JSONObject jsonObject = new JSONObject();
-//        jsonObject.put("msg","调用成功");
-//        jsonObject.put("code","0000");
-//        JSONObject jsonData = new JSONObject();
-//        try {
-//            subject.login(token);
-//            User userComplete = userService.selectByName(user.getUsername());
-//            request.setAttribute("user",user);
-//            request.getSession().setAttribute("user",userComplete);
-//            request.getSession().setAttribute("message",NewWebSocket.wSocketMessageListCenter.size());
-//            jsonData.put("judge","0");
-//        }catch (Exception e){
-//            //这里将异常打印关闭是因为如果登录失败的话会自动抛异常
-//            jsonData.put("judge","-1");
-//        }
-//        jsonObject.put("data",jsonData);
-//        return jsonObject.toString();
+    @RequestMapping(value="/login",method = RequestMethod.POST,consumes = "application/json")
+    @ResponseBody
+    public String login(@RequestBody User user, HttpServletRequest request){
         //调用login方法来验证是否是注册用户
         boolean loginType = userService.login(user.getUsername(),user.getPassword());
 
@@ -72,35 +53,6 @@ public class LoginController {
         jsonObject.put("data",jsonData);
         return jsonObject.toString();
     }
-
-//    /**
-//     * 登录
-//     * @param user
-//     * @param request
-//     * @return
-//     */
-//    @ApiOperation(value = "注册", notes = "注册notes", produces = "application/json")
-//    @RequestMapping(value="/login",method = RequestMethod.POST,consumes = "application/json")
-//    public String login(@RequestBody User user, HttpServletRequest request){
-//        //调用login方法来验证是否是注册用户
-//        boolean loginType = userService.login(user.getUsername(),user.getPassword());
-//
-//        JSONObject jsonObject = new JSONObject();
-//        jsonObject.put("msg","调用成功");
-//        jsonObject.put("code","0000");
-//        JSONObject jsonData = new JSONObject();
-//        if(loginType){
-//            //如果验证通过,则将用户信息传到前台
-//            request.setAttribute("user",user);
-//            request.getSession().setAttribute("user",user);
-//            jsonData.put("user",user);
-//            jsonData.put("judge","0");
-//        }else{
-//            jsonData.put("judge","-1");
-//        }
-//        jsonObject.put("data",jsonData);
-//        return jsonObject.toString();
-//    }
 
     /**
      * 注销
