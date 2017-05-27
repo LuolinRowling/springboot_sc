95d9d0adf0e1ed09d3b797e9c043180d6315551f
12301068JiangDongyu <12301068@bjtu.edu.cn>
2017-05-28 00:00:29 +0800

    update

--- src/main/java/com/pku/Application.java
+++ src/main/java/com/pku/Application.java
@@ -2,19 +2,11 @@ package com.pku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+public class Application{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
